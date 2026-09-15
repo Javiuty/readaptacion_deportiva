cb948d0abb5e5eefa1bfc9acd6939b8a60a3bccf
JEscuadra <javiiescuadra@gmail.com>
2026-09-15 10:25:37 +0200

    feat: new design

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 export const PHONE_DISPLAY = "641482231";
 export const PHONE_TEL = "+34641482231";
 export const WHATSAPP_URL =
-  "https://wa.me/34641482231?text=Hola%20Carmen%2C%20me%20gustar%C3%ADa%20informarme%20sobre%20tus%20entrenamientos.";
+  "https://wa.me/34641482231?text=Hola%20Nerea%2C%20me%20gustar%C3%ADa%20informarme%20sobre%20tus%20entrenamientos.";
 
 export const NAV_LINKS = [
   { href: "#servicios", label: "Servicios" },
